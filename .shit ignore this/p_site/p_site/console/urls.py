0cd1e6afa054f0c92f69b25a32db8dcc63d0c7ff
dia <124006582+diamondTheRat@users.noreply.github.com>
2024-07-17 01:26:15 +0300

    removed django attempt

--- .shit ignore this/p_site/p_site/console/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-import console.views as views
-import os
-urlpatterns = [
-    path("", views.console.as_view())
-]
--- .shit ignore this/p_site/p_site/console/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, HttpResponse
-from django.views.generic import TemplateView
-
-# Create your views here.
-
-
-class console(TemplateView):
-    template_name = "console/index.html"
